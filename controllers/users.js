aa7222e7d42c628b58d28fc63b6d0ba3bb5cf788
angad <angad.kumar.min20@itbhu.ac.in>
2024-09-16 18:53:14 +0530

    20th commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,23 @@
 const User = require('../models/user');
 
 module.exports.renderRegister = (req, res) => {
-    res.render('users/register');
+    return res.render('users/register');
 }
 
 module.exports.register = async (req, res, next) => {
     try {
-        return res.send(process.env.MONGO_URI)
+        // return res.send(process.env.MONGO_URI)
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
+            return res.redirect('/campgrounds');
         })
     } catch (e) {
         req.flash('error', e.message);
-        res.redirect('register');
+        return res.redirect('register');
     }
 }
 
